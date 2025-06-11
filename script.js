501e2e87d83d2c9f2a70d2f721c028094478e19d
Nathan Lucca <nathan.lucca11@gmail.com>
2025-06-11 17:38:43 -0300

    commit function set budget disable edit and modify list

---
+++ script.js
@@ -0,0 +1,58 @@
+let totalAmount = document.getElementById("total-amount");
+let userAmount = document.getElementById("user-amount");
+const checkAmountButton = document.getElementById("check-amount")
+const totalAmountButton = document.getElementById("total-amount-button")
+const productTitle = document.getElementById("product-title")
+const errorMessage = document.getElementById("budget-error")
+const productTitleError = document.getElementById("product-title-error")
+const productCostError = document.getElementById("product-cost-error")
+const amount = document.getElementById("amount")
+const expenditureValue = document.getElementById("expenditure-value")
+const balanceValue = document.getElementById("balance-amount")
+const list = document.getElementById("list")
+let tempAmount = 0;
+
+// Set Budget Part
+totalAmountButton.addEventListener("click", ()=>{
+    tempAmount = totalAmount.ariaValueMax;
+    //empty or negative input
+    if(tempAmount === "" || tempAmount < 0){
+        errorMessage.classList.remove("hide")
+    }else{
+        errorMessage.classList.add("hide")
+        // Set Budget
+        amount.innerHTML = tempAmount
+        //Set Balance
+        balanceValue.innerText = tempAmount -
+        expenditureValue.innerText;
+        //Clear Input Box
+        totalAmountButton = "";
+    }
+})
+
+//Function To Disable Edit and Delete Button
+
+const disableButtons = (bool) => {
+    let editButtons = document.getElementsByClassName("edit")
+    Array.from(editButtons).forEach(element => {
+        element.disabled = bool
+    })
+}
+
+// Function To Modify List Elements
+
+const modifyElement = (element, edit=false) =>{
+    let parentDiv = element.parentElement
+    let currentBalance = balanceValue.innerText
+    let currentExpense = expenditureValue.innerText
+    let parentAmount = parentDiv.querySelector(".amount").innerText;
+    if(edit){
+        let parentText = parentDiv.querySelector(".product").innerHTML
+        productTitle.value = parentText
+        userAmount.value = parentAmount
+        disableButtons(true)
+    }
+    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount)
+    expenditureValue.innerText = parseInt(currentExpense) - parseInt(parentAmount)
+    parentDiv.remove()
+}
